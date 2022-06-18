102d697fb7f65a78f6f8faaccca1458187ff0db7
razdva0 <90208494+razdva0@users.noreply.github.com>
2022-06-18 20:26:03 +0300

    huita

---
+++ Assets/Editor/RotateToCursor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToCursor : MonoBehaviour
+{
+    private Vector3 mousePos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
